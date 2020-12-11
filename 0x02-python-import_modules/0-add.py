ac6c1cac11be27ca006cae266b2739ef6caf5c41
William Colindres <1689@holbertonschool.com>
2020-12-11 01:01:11 +0000

    imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__":
+    import add_0
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
