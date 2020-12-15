564f0370d7165594b71b3b094af8b3c8d6a75dd7
William Colindres <1689@holbertonschool.com>
2020-12-15 19:43:37 +0000

    function that prints all integers of a list.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if not my_list:
-        print("Empty list!")
-    else:  
-        print('{0}\n{1}\n{2}\n{3}\n{4}'.format(*my_list))
+    if my_list:
+        for i in my_list:
+            print("{:d}".format(i))
